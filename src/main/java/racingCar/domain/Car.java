79cef9942833cc22ae8ada030d143c58eac636b4
sunba <sunba2318@gmail.com>
2023-03-10 18:04:54 +0900

    feat : Car 클래스 구현

---
+++ src/main/java/racingCar/domain/Car.java
@@ -0,0 +1,36 @@
+package racingCar.domain;
+
+public class Car {
+    private final String name;
+    private Position position;
+
+    private static final Integer MINIMUM_NUMBER_TO_MOVE = 4;
+
+    public Car(String name) {
+        this(name, 0);
+    }
+
+    public Car(String name, Integer position) {
+        this.name = validate(name);
+        this.position = new Position(position);
+    }
+
+    public void run(int randomNo) {
+        if (randomNo >= MINIMUM_NUMBER_TO_MOVE) {
+            this.position = position.move();
+        }
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private String validate(String name) {
+        if (name.length() > 5) throw new IllegalArgumentException("이름은 5자를 초과할 수 없음");
+        return name;
+    }
+}
---
+++ src/test/java/racingCar/CarTest.java
@@ -0,0 +1,31 @@
+package racingCar;
+
+import org.junit.jupiter.api.Test;
+import racingCar.domain.Car;
+import racingCar.domain.Position;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CarTest {
+
+
+    @Test
+    void _4이상_전진() {
+        Car car = new Car("a");
+        car.run(4);
+        assertThat(car.getPosition()).isEqualTo(new Position(1));
+    }
+
+    @Test
+    void _3이하_가만히() {
+        Car car = new Car("a");
+        car.run(3);
+        assertThat(car.getPosition()).isEqualTo(new Position(0));
+    }
+
+    @Test
+    void 이름은_5자를_초과할수_없음() {
+        assertThatThrownBy(() -> new Car("abcdef")).isInstanceOf(IllegalArgumentException.class);
+    }
+}
